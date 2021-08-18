71e3dab1f5dbdeb701300818d31ec6040aac0162
eballantine <lizzy.kayballantine@gmail.com>
2021-08-18 16:22:54 +0100

    fixed failing tests. Testing uses test db

---
+++ spec/bookmark_spec.rb
@@ -0,0 +1,20 @@
+require 'bookmark'
+
+describe Bookmark do 
+  describe '.all' do
+    it 'returns an array of bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      # Add the test data
+      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
+      bookmark = Bookmark.all
+
+      expect(bookmark).to include "http://www.makersacademy.com"
+      expect(bookmark).to include "http://www.destroyallsoftware.com"
+      expect(bookmark).to include "http://www.google.com"
+    end
+  end
+end
